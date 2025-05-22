6f6828784864f54a536830dc7bc96b40ce94013b
Brian <brianhenriquedp@gmail.com>
2025-05-22 13:42:18 -0400

    22.05.25

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-// Usando console
-console.log('Usando o console');
-console.log('Olá');
+let nome = 'Clark Kent';
+let email = 'clarkent@fiap.com'
+let idade = 35;
+let logado = true;
+
+// exibindo variaveis
+console.log(nome);
+console.log(email);
+console.log(idade);
+console.log(logado);
+
+// exibindo o tipo
+console.log(typeof nome);
+console.log(typeof email);
+console.log(typeof idade);
+console.log(typeof logado);
+
+const DATA_NASCIMENTO = '1988-10-01';
