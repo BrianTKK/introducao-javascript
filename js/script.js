ac9e68e713e7a2b93759b1f6d6bfaf9464d38ee8
Brian <brianhenriquedp@gmail.com>
2025-05-29 13:16:57 -0400

    Sorteando com JS

--- js/script.js
+++ js/script.js
@@ -1,33 +1,11 @@
-let numero = 23.53;
-console.log(numero);
+let numeroSorteado = Math.random();
+numeroSorteado *= 100;
 
-// Math ceil - Arredonda o numero para cima
-console.log(`Math.ceil(${numero}) = ${Math.ceil(numero)}`);
+console.log(`Número sorteado: ${numeroSorteado}`);
 
-// Math floor - Arredonda o numero para baixo
-console.log(`Math.floor(${numero}) = ${Math.floor(numero)}`);
+numeroSorteado = parseInt(numeroSorteado);
+console.log(`Número sorteado: ${numeroSorteado}`);
 
-// Math round - Arredonda o numero de acordo com a casa decimal
-console.log(`Math.round(${numero}) = ${Math.round(numero)}`);
-
-// Math max - Retorna o maior valor entre os argumentos
-console.log(`Math.max(${numero}) = ${Math.max(34, 84, 10, 25, 99, 74, 86)}`);
-
-// Math min - Retorna o menor valor entre os argumentos
-console.log(`Math.min(${numero}) = ${Math.min(34, 84, 10, 25, 99, 74, 86)}`);
-
-// parseInt - Retorna apenas o número inteiro
-console.log(`parseInt(${numero}) = ${parseInt(numero)}`);
-
-// Math pow - Retorna o valor de um número elevado a uma potência
-console.log(`Math.pow(${numero} a segunda potencia) = ${Math.pow(numero, 2)}`);
-console.log(numero ** 2); // Outra forma de fazer a potenciação
-
-// Math random - Retorna um número aleatório entre 0 e 1
-console.log(Math.random());
-
-// Math sqrt - Retorna a raiz quadrada de um número
-console.log(Math.sqrt(16));
-
-// Math cbrt - Retorna a raiz cúbica de um número
-console.log(Math.cbrt(27));
+// Simplificando o código
+let novoSorteio = Math.floor(Math.random() * 100);
+console.log(`Novo número sorteado: ${novoSorteio}`);
