64cc6234c2ea8916454becc54a1cc22cea1ae4df
Brian <brianhenriquedp@gmail.com>
2025-06-30 11:12:56 -0400

    If / Else
    Substitui o exemplo anterior do objeto Date por uma nova lógica que demonstra operações if/else e ternárias para precificação de produtos, métodos de pagamento e status do estoque.

--- js/script.js
+++ js/script.js
@@ -1,74 +1,26 @@
-let dataAtual = new Date();
-console.log(dataAtual);
+const precoProduto = 150;
+const frete = 30;
+let valorFinal = 0;
 
-// data do mes
-let dataMes = dataAtual.getDate();
-if (dataMes < 10) {
-	dataMes = '0' + dataMes;
-}
-console.log(dataMes);
-
-// dia da semana
-let dataDia = dataAtual.getDay();
-console.log(dataDia);
-
-// colocando o dia da semana por extenso
-switch (dataDia) {
-	case 0:
-		console.log('Domingo');
-		break;
-
-	case 1:
-		console.log('Segunda-feira');
-		break;
-
-	case 2:
-		console.log('Terça-feira');
-		break;
-
-	case 3:
-		console.log('Quarta-feira');
-		break;
-
-	case 4:
-		console.log('Quinta-feira');
-		break;
-
-	case 5:
-		console.log('Sexta-feira');
-		break;
-
-	default:
-		console.log('Sábado');
+if (precoProduto < 150) {
+	valorFinal = precoProduto + frete;
+} else {
+	valorFinal = precoProduto;
 }
 
-// mes
-let mesAtual = dataAtual.getMonth() + 1; // Janeiro é 0
-if (mesAtual < 10) {
-	mesAtual = '0' + mesAtual;
+console.log(`O valor total do produto é R$ ${valorFinal.toFixed(2)}`);
+
+let metodoPagamento = 'Pix';
+if (metodoPagamento === 'Cartão de Crédito') {
+    console.log('Você pode parcelar em até 12x');
+}else if (metodoPagamento === 'Boleto') {
+    console.log('Você terá 5% de desconto no boleto');
+}else if (metodoPagamento === 'Pix') {
+    console.log('Você terá 10% de desconto no Pix');
+}else {
+    console.log('Outro meio de pagamento');
 }
-console.log(mesAtual);
 
-// pegando o ano
-anoAtual = dataAtual.getFullYear();
-console.log(anoAtual);
-
-// pegando a hora
-let horaAtual = dataAtual.getHours();
-horaAtual = horaAtual < 10 ? '0' + horaAtual : horaAtual;
-console.log(horaAtual);
-
-// pegando minutos
-let minutoAtual = dataAtual.getMinutes();
-minutoAtual = minutoAtual < 10 ? '0' + minutoAtual : minutoAtual;
-console.log(minutoAtual);
-
-// pegando segundos
-let segundoAtual = dataAtual.getSeconds();
-segundoAtual = segundoAtual < 10 ? '0' + segundoAtual : segundoAtual;
-console.log(segundoAtual);
-
-// montando a data
-console.log(
-	`Data atual: ${dataMes}/${mesAtual}/${anoAtual} ${horaAtual}:${minutoAtual}:${segundoAtual}`
-);
+let estoque = 5;
+let statusEstoque = estoque > 0 ? 'Produto disponível' : 'Produto esgotado';
+console.log(statusEstoque);
